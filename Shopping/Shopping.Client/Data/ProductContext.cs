56b27ad34c093142cf85fc32ff757ed8ef663dc9
Dhanashri Deshmukh <dhanashrideshmukh146@gmail.com>
2023-10-04 09:44:13 +0530

    message

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,45 @@
+﻿using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class  ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+
+            new Product()
+            {
+                Name = "Iphone x",
+                Description = "new iphone",
+                ImageFile = "product-1.png",
+                Price = 70000,
+                Category = "Smart phone"
+            },
+
+            new Product()
+            {
+                Name = "android",
+                Description = "new iphone",
+                ImageFile = "product-1.png",
+                Price = 80000,
+                Category = "Smart phone"
+            },
+            new Product()
+            {
+                Name = "Xiamio Mi 9",
+                Description = "new iphone",
+                ImageFile = "product-1.png",
+                Price = 47000,
+                Category = "White Appliances"
+            },
+            new Product()
+            {
+                Name = "LG g7 fridge",
+                Description = "new iphone",
+                ImageFile = "product-1.png",
+                Price = 80000,
+                Category = "Home Kitchen"
+            }
+        };
+    }
+}
